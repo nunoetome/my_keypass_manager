6eee73320965735d548553c749d74b11315a9cd0
Nuno Tomé <nunotomeit@gmail.com>
2025-08-04 23:09:02 +0100

    Refactor comments and improve file existence check in renomear_campos function

--- app.py
+++ app.py
@@ -3,18 +3,43 @@ import os
 
 def renomear_campos():
     # Acede ao ficheiro .kdbx
-   # Caminho absoluto para o ficheiro Main.kdbx na mesma pasta do script
+    # Caminho absoluto para o ficheiro Main.kdbx na mesma pasta do script
     base_dir = os.path.dirname(os.path.abspath(__file__))
     kdbx_path = os.path.join(base_dir, 'Main.kdbx')
+
     senha = 'asdasdsad'  # ou keyfile, se usares
     string_to_change_before = '_template'  # string a ser substituída
     string_to_change_after= '_t'  # string nova
-    
 
+    # lista de custom_propertys a serem ignoradas
+    ignore_list = [
+        '_etm_template'
+        ]
+    #lista de substrings a serem ignoradas
+    ignore_substrings = [
+        'etm_template',
+        'etm_t'
+        ]
+    
+    # Verifica se o ficheiro existe
+    if not os.path.exists(kdbx_path):
+        print(f'Ficheiro {kdbx_path} não encontrado.')
+        return
     kp = PyKeePass(kdbx_path, password=senha)
+
+    # Verifica se o ficheiro foi carregado corretamente
+    if kp is None:
+        print('Erro ao carregar o ficheiro .kdbx. Verifique a senha ou o caminho.')
+        return
+    print(f'Ficheiro {kdbx_path} carregado com sucesso.')
+
 
     modificados = 0
 
+    # Itera sobre todas as entradas do KeePass
+    # Se quiseres filtrar por grupo, podes usar kp.find_groups(name='nome_do_grupo')
+    # ou kp.find_entries(title='nome_da_entrada') para entradas específicas
+    # Aqui, iteramos por todas as entradas
     for entry in kp.entries:
         # Copia da dict original para iterar com segurança
         for custom_property in list(entry.custom_properties.keys()):
